b174bc8d07efd9f02cce1695a7cbf182abacb387
Aaron <aaron@regtransfers.co.uk>
2022-06-01 16:46:09 +0100

    Custom build steps added. Compression for two alg added.

---
+++ Angular_Optimized/custom-webpack.config.js
@@ -0,0 +1,32 @@
+﻿const CompressionPlugin = require(`compression-webpack-plugin`);
+const BrotliPlugin = require(`brotli-webpack-plugin`);
+const path = require(`path`);
+const zlib = require("zlib");
+module.exports = {
+  plugins:[
+/*    new BrotliPlugin({
+      asset: '[fileWithoutExt].[ext].br',
+      test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/
+    }),*/
+    new CompressionPlugin({
+      filename: "[path][base].gz",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+    new CompressionPlugin({
+      filename: "[path][base].br",
+      algorithm: "brotliCompress",
+      test: /\.(js|css|html|svg|woff|woff2|ttf|eot)$/,
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+      threshold: 10240,
+      minRatio: 0.8,
+      deleteOriginalAssets: false,
+    })
+  ],
+}
